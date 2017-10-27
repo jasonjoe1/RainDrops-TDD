d9fa7231f8305fdb2648d83e309a957d3a01e603
jason <appletedd@yahoo.com>
2017-10-27 10:39:45 +0100

    Func(rainDrops): Refactored code using es6 syntaxes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
-'use strict'
-
-module.exports ={
-    RainDrops: (number) =>{
-        let result ="" ;
-        if (number % 3 === 0) {
-            result += 'Pling';
-        }
-        if (number % 5 === 0) {
-            result += 'Plang';
-              }
-              if (number % 7 === 0) {
-                  result += 'Plong';
-              }
-              if (number%3!==0 && number%5!==0 && number%7!==0){
-                return number;
-              }
-              if (result === '') {
-                return number.toString();
-              }else {
-                  return result;
-              }
-        return result;
+export default{
+  RainDrops: (number) => {
+    let result = '';
+    if (number % 3 === 0) {
+      result += 'Pling';
     }
+    if (number % 5 === 0) {
+      result += 'Plang';
+    }
+    if (number % 7 === 0) {
+      result += 'Plong';
+    }
+    if (number % 3 !== 0 && number % 5 !== 0 && number % 7 !== 0) {
+      return number;
+    }
+    if (result === '') {
+      return number.toString();
+    }
+    return result;
+
+    return result;
+  }
 };
